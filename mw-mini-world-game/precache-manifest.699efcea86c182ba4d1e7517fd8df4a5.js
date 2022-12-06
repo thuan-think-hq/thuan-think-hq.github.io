a7d9802264b1e4f0834aa41733d47366f662e75b
thuan.l <thuan.l@think-hq.com.au>
2022-12-06 14:22:23 +1100

    .

---
+++ mw-mini-world-game/precache-manifest.699efcea86c182ba4d1e7517fd8df4a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b85d41c0a8a24f372cd53745117d60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f5736daede6f2639c33",
+    "url": "/static/css/main.6fbf8871.chunk.css"
+  },
+  {
+    "revision": "a777f65569450766e5aa",
+    "url": "/static/js/2.caa111ac.chunk.js"
+  },
+  {
+    "revision": "c3431a4d658e0e7a6d0ca41518717a69",
+    "url": "/static/js/2.caa111ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bed63ff2be781e9eab29",
+    "url": "/static/js/3.41fcbf3c.chunk.js"
+  },
+  {
+    "revision": "1f5736daede6f2639c33",
+    "url": "/static/js/main.0f3466c9.chunk.js"
+  },
+  {
+    "revision": "7d8c5bfc6f059a50b64b",
+    "url": "/static/js/runtime-main.8a099fd8.js"
+  }
+]);
